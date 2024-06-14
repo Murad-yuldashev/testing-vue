8333fadfd41dc5a62e9f151f5267c28780620a3b
Murod-de <murad101693@gmail.com>
2024-06-14 21:54:00 +0500

    init

---
+++ src/components/Create.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="addTodo()">
+    <div class="grid grid-cols-1 gap-4">
+      <div>
+        <input
+          class="border-2 outline-none py-2 px-2 shadow-md font-medium w-full rounded border-blue-300 hover:border-blue-600 focus:border-blue-600 focus:shadow-blue-200"
+          v-model="newTodo"
+        />
+      </div>
+      <div>
+        <div class="grid grid-cols-2 gap-2">
+          <div>
+            <button
+              class="text-white py-2 px-4 shadow-md w-full rounded bg-red-400 hover:bg-red-600 font-semibold"
+              v-if="todos.length !== 0"
+              @click="removeAllTodos"
+            >
+              Remove All Todos
+            </button>
+          </div>
+          <div>
+            <button
+              class="text-white py-2 px-4 shadow-md w-full rounded bg-blue-400 hover:bg-blue-600 font-semibold"
+            >
+              Add Todo
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script setup></script>
---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<!-- 1
+ CSS Grid: Ikki o'lchovli ishlaydi — qatorlar va ustunlar. Bu Grid'ni gorizontal va vertikal elementlarni boshqarish kerak bo'lgan murakkab maketlar yaratishda ideal qiladi.
+ Flexbox: Bir o'lchovli ishlaydi — ya'ni qator yoki ustun. Bu Flexbox'ni bitta yo'nalishda (masalan, gorizontal yoki vertikal) elementlarni tartiblash uchun juda mos qiladi. 
+ 2
+ hooklar yordamida komponentlar yuklanganda, yangilanganida yoki yo'q qilinganda kerakli kodni bajarish mumkin
+ 1 beforeMount: Komponent DOM ga qo'shilishidan oldin chaqiriladi.
+ 2 mounted: Komponent DOM ga qo'shilgandan keyin chaqiriladi.
+ 3 beforeUpdate: Reaktiv data yangilanishidan oldin chaqiriladi.
+ 4 updated: Reaktiv data yangilangandan keyin chaqiriladi.
+
+3
+asyncData metodi Nuxt.js-da sahifa yoki komponent uchun ma'lumotlarni server tomonida olish va ularga sahifa yuklanishidan oldin kirish imkonini berish uchun ishlatiladi.
+ sahifa yuklanishidan oldin asinxron ravishda ma'lumotlarni olish imkonini beradi
+ -->
